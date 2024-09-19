1e847b32c462c3f4022398a94ec60cd4b039ef81
LemonFlavoredStudent <micah.vickery@g.austincc.edu>
2024-09-19 21:01:45 +0000

    HW 3 Work

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,34 @@
 //write include statement for decisions header
-
+#include "decisions.h"
 
 //Write code for function(s) code here
+string get_letter_grade_using_if(int grade)
+{
+    string letter_grade = "";
+
+    if (grade <= 100 && grade >= 90)
+    {
+        letter_grade = "A";
+    }
+    else if (grade <= 89 && grade >= 80)
+    {
+        letter_grade = "B";
+    }
+    else if (grade <= 79 && grade >= 70)
+    {
+        letter_grade = "C";
+    }
+    else if (grade <= 69 && grade >= 60)
+    {
+        letter_grade = "D";
+    }
+    else if (grade <= 59 && grade >= 0)
+    {
+        letter_grade = "F";
+    }
+    else
+    {
+        letter_grade = "Invalid Value";
+    }
+    return letter_grade;
+}
