f7f2489364566464a5a13ba76e61c4c549f1671c
LemonFlavoredStudent <micah.vickery@g.austincc.edu>
2024-09-27 23:58:45 +0000

    hw 4 changes

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,7 +1,7 @@
 //write include statements
-
+#include "repetition.h"
 //write using statements
-
+using std::cout; using std::cin;
 int main() 
 {
 	return 0;
--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,34 @@
 //add include statements
-
+#include "repetition.h"
 //add function(s) code here
+int factorial(int num)
+{
+    auto total = 1;
+
+    while (num > 0)
+    {
+        total = num*total;
+        num--;
+    }
+
+    return total;
+}
+
+int gcd(int num1, int num2)
+{
+    auto temp = 0;
+    while (num1 != num2)
+    {
+        if (num1<num2)
+        {
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+        else if (num1>num2)
+        {
+            num1=num1-num2;
+        }
+    }
+    return num1;
+}
