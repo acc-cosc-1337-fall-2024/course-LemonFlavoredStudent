e6814c3d960dde85ae8ac2177989345bbadbdf35
LemonFlavoredStudent <micah.vickery@g.austincc.edu>
2024-10-04 22:04:54 +0000

    HW 5 Changes

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,7 @@
 //add include statements
-
+#include "func.h"
 //add function code here
+double get_gc_content(const string& dna)
+{
+    
+}
--- src/homework/05_functions/func.h
+++ src/homework/05_functions/func.h
@@ -1,1 +1,8 @@
+#include <iostream>
+#include <string>
+using std::string;
+
 //add function(s) prototype here
+double get_gc_content(const string& dna);
+string get_dna_compliment(string dna);
+string reverse_string(string dna);
