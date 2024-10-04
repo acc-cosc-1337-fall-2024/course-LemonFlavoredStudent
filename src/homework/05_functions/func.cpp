88414b235cac14f18ea1dce14b9b40dfd2e09546
LemonFlavoredStudent <micah.vickery@g.austincc.edu>
2024-10-04 22:43:20 +0000

    HW 5 Changes

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -3,5 +3,16 @@
 //add function code here
 double get_gc_content(const string& dna)
 {
-    
+    double amount = 0;
+    char checkCharacter1 = 'C';
+    char checkCharacter2 = 'G';
+    for (int i = 0; i<dna.size(); i++)
+    {
+        if (dna[i] == checkCharacter1 or dna[i] == checkCharacter2)
+        {
+            amount++;
+        }
+
+    }
+    return amount/dna.size();
 }
