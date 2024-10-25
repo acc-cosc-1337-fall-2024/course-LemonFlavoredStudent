85a85d9e56fa6b590ac5bb7b99e2417a30977342
LemonFlavoredStudent <micah.vickery@g.austincc.edu>
2024-10-25 23:24:45 +0000

    tictactoe changes

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,24 @@
+#include "tic_tac_toe.h"
+#include <iostream>
+#include <string>
+using std::cout; using std::cin;
+using std::string;
+
 
 int main() 
 {
+	TicTacToe game;
+	string first_player;
+	
+	do
+	{
+		cout<<"Enter first player: ";
+		cin>>first_player;
+
+		
+		
+	} while (/* condition */);
+	
 
 	return 0;
 }
