5b16805c1edd6d57c01b91d100800a8c1221e2c9
LemonFlavoredStudent <micah.vickery@g.austincc.edu>
2024-11-01 22:02:56 +0000

    HW 7 Test Cases

--- test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
+++ test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
@@ -64,3 +64,122 @@ TEST_CASE("Test win by first column")
 	REQUIRE(game.game_over()==true);
 	REQUIRE(game.get_winner()=="X");
 }
+TEST_CASE("Test win by second column")
+{
+	TicTacToe game;
+	
+	game.start_game("X");
+	game.mark_board(2);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(1);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(5);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(3);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(8);
+	REQUIRE(game.game_over()==true);
+	REQUIRE(game.get_winner()=="X");
+}
+TEST_CASE("Test win by third column")
+{
+	TicTacToe game;
+	
+	game.start_game("X");
+	game.mark_board(3);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(2);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(6);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(5);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(9);
+	REQUIRE(game.game_over()==true);
+	REQUIRE(game.get_winner()=="X");
+}
+TEST_CASE("Test win by first row")
+{
+	TicTacToe game;
+	
+	game.start_game("X");
+	game.mark_board(1);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(6);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(2);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(5);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(3);
+	REQUIRE(game.game_over()==true);
+	REQUIRE(game.get_winner()=="X");
+}
+TEST_CASE("Test win by second row")
+{
+	TicTacToe game;
+	
+	game.start_game("X");
+	game.mark_board(4);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(1);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(5);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(9);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(6);
+	REQUIRE(game.game_over()==true);
+	REQUIRE(game.get_winner()=="X");
+}
+TEST_CASE("Test win by third row")
+{
+	TicTacToe game;
+	
+	game.start_game("X");
+	game.mark_board(7);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(6);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(8);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(5);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(9);
+	REQUIRE(game.game_over()==true);
+	REQUIRE(game.get_winner()=="X");
+}
+TEST_CASE("Test win diagonally from top left")
+{
+	TicTacToe game;
+	
+	game.start_game("X");
+	game.mark_board(1);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(6);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(5);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(4);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(9);
+	REQUIRE(game.game_over()==true);
+	REQUIRE(game.get_winner()=="X");
+}
+TEST_CASE("Test win diagonally from bottom left")
+{
+	TicTacToe game;
+	
+	game.start_game("X");
+	game.mark_board(7);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(6);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(5);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(4);
+	REQUIRE(game.game_over()==false);
+	game.mark_board(3);
+	REQUIRE(game.game_over()==true);
+	REQUIRE(game.get_winner()=="X");
+}
