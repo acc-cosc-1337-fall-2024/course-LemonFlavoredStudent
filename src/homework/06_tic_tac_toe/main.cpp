d5f937e9684aa9ee593dde5c03b46c3d67a7518f
LemonFlavoredStudent <micah.vickery@g.austincc.edu>
2024-11-14 23:08:00 +0000

    HW 9 Changes

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,14 +1,16 @@
 #include "tic_tac_toe.h"
 #include "tic_tac_toe_manager.h"
+#include "tic_tac_toe_3.h"
+#include "tic_tac_toe_4.h"
 #include <iostream>
 #include <string>
 using std::cout; using std::cin;
 using std::string;
-
+using std::unique_ptr; using std::make_unique;
 
 int main() 
 {
-	TicTacToe game;
+	unique_ptr<TicTacToe> game;
 	string first_player;
 	char user_choice = 'y';
 	TicTacToeManager manager;
@@ -16,6 +18,21 @@ int main()
 	
 	do
 	{
+		auto game_type = 0;
+
+		cout<<"Enter game type(3 or 4): ";
+		cin>>game_type;
+
+		if (game_type == 3)
+		{
+			game = make_unique<TicTacToe3>();
+		}
+		else
+		{
+			game = make_unique<TicTacToe4>();
+		}
+
+
 		cout<<"Enter first player (X or O): ";
 		cin>>first_player;
 
@@ -25,27 +42,27 @@ int main()
 			cin>>first_player;
 		}
 
-		game.start_game(first_player);
+		game->start_game(first_player);
 
 		int position;
 
-		while (!game.game_over())
+		while (!game->game_over())
 		{
 			cout<<"Enter a position: ";
 			cin>>position;
-			game.mark_board(position);
-			game.display_board();
+			game->mark_board(position);
+			game->display_board();
 		}
 
-		if (game.get_winner()=="C")
+		if (game->get_winner()=="C")
 		{
 			cout<<"The game ended in a tie!\n";
 		}
 		else 
 		{
-			cout<<game.get_winner()<<" is the winner!\n";
+			cout<<game->get_winner()<<" is the winner!\n";
 		}
-		manager.save_game(game);
+		manager.save_game(*game);
 		manager.get_winner_total(o,x,t);
 		cout<<"Current Score: O has won "<<o<<" times, X has won "<<x<<" times, and there has been "<<t<<" ties.\n";
 
