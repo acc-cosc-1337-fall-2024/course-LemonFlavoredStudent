bcfde2d867aa2aa4824596a0d84b6229b25468b3
LemonFlavoredStudent <micah.vickery@g.austincc.edu>
2024-11-14 22:18:05 +0000

    HW 9 Changes

--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -8,6 +8,7 @@
 class TicTacToe
 {
 public:
+    TicTacToe(int size) : pegs(size*size," "){}
     bool game_over();
     void start_game(std::string first_player);
     void mark_board(int position);
@@ -15,13 +16,15 @@ public:
     void display_board() const;
     std::string get_winner() {return winner;};
 
+protected:
+    std::vector<std::string> pegs;
+    virtual bool check_column_win();
+    virtual bool check_row_win();
+    virtual bool check_diagonal_win();
+
 private:
     std::string winner;
     std::string player;
-    std::vector<std::string> pegs{9, " "};
-    bool check_column_win();
-    bool check_row_win();
-    bool check_diagonal_win();
     void clear_board();
     void set_next_player();
     bool check_board_full();
--- src/homework/06_tic_tac_toe/tic_tac_toe_4.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe_4.cpp
@@ -10,9 +10,20 @@ Win by column if and return true if (each column index)
 else
 false
 */
-
-
-
+bool TicTacToe4::check_column_win()
+{
+    if ((pegs[0] != " " && pegs[0]==pegs[4] && pegs[4]==pegs[8] && pegs[8]==pegs[12])||
+    (pegs[1] != " " && pegs[1]==pegs[5] && pegs[5]==pegs[9] && pegs[9]==pegs[13])||
+    (pegs[2] != " " && pegs[2]==pegs[6] && pegs[6]==pegs[10] && pegs[10]==pegs[14])||
+    (pegs[3] != " " && pegs[3]==pegs[7] && pegs[7]==pegs[11] && pegs[11]==pegs[15]))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
 /*
 class function check_row_win
@@ -23,7 +34,20 @@ Win by row if
 12,13,14, 15 are equal
 */
 
-
+bool TicTacToe4::check_row_win()
+{
+    if ((pegs[0] != " " && pegs[0]==pegs[1] && pegs[1]==pegs[2] && pegs[2]==pegs[3])||
+    (pegs[4] != " " && pegs[4]==pegs[5] && pegs[5]==pegs[6] && pegs[6]==pegs[7])||
+    (pegs[8] != " " && pegs[8]==pegs[9] && pegs[9]==pegs[10] && pegs[10]==pegs[11])||
+    (pegs[12] != " " && pegs[12]==pegs[13] && pegs[13]==pegs[14] && pegs[14]==pegs[15]))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
 /*
 class function check_diagonal_win
@@ -34,3 +58,15 @@ Win diagonally
 12,13,14, 15
 
 */
+bool TicTacToe4::check_diagonal_win()
+{
+    if ((pegs[0] != " " && pegs[0]==pegs[5] && pegs[5]==pegs[10] && pegs[10]==pegs[15])||
+    (pegs[3] != " " && pegs[3]==pegs[6] && pegs[6]==pegs[9] && pegs[9]==pegs[12]))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
